671bba9ba24e17e2b9b39048406e96b2a62118a1
3AeMe3 <fm792086@gmail.com>
2024-05-14 14:49:02 -0500

    INPUT SYSTEM REFACTOR

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,37 +7,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float speedMovement = 7f;
-
+    [SerializeField] GameInput gameInput;
 
     private bool isWalking; 
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0f,0f);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;  
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-
-        }
-
-
-         inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x ,0,inputVector.y);
 
         transform.position += moveDir * Time.deltaTime * speedMovement;
